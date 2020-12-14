85ab96905fd02e0156582d4f536354f885a1ddac
epi <43392618+epi052@users.noreply.github.com>
2020-12-14 16:47:36 -0600

    Update feroxbuster.rb

--- feroxbuster.rb
+++ feroxbuster.rb
@@ -4,7 +4,7 @@ class Feroxbuster < Formula
   desc 'Fast, simple, recursive content discovery tool written in Rust. 🦀'
   homepage 'https://github.com/epi052/feroxbuster'
   url 'https://github.com/epi052/feroxbuster/releases/download/v1.10.1/x86_64-macos-feroxbuster.tar.gz'
-  sha256 '9fb610a5cab2b6972e5b884f1b30ec1c5a63d744f03e17f6f5d381567ccf61c0'
+  sha256 '0ab546038958fb227aecdc642b42ee44dbd70b27b058232d6f74d0fa69ca31de'
 
   resource 'ferox-config' do
     url 'https://raw.githubusercontent.com/epi052/feroxbuster/master/ferox-config.toml.example'
